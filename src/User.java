c7493adc14bc8a6cfca2674190728086e288a118
palm000palmovich <ivankiselev6534@gmail.com>
2024-09-25 08:38:10 +0300

    all_structures_Without_Functions

---
+++ src/User.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private int age;
+    private List<Movie> bestFilms = new ArrayList<>();
+    private List<Movie> watchLater = new ArrayList<>();
+
+
+    //Конструктор
+    public User(int age, List<Movie> bestFilms, List<Movie> watchLater) {
+        this.age = age;
+        this.bestFilms = bestFilms;
+        this.watchLater = watchLater;
+    }
+
+    //Геттеры и сеттеры
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Movie> getBestFilms() {
+        return bestFilms;
+    }
+
+    public void setBestFilms(List<Movie> bestFilms) {
+        this.bestFilms = bestFilms;
+    }
+
+    public List<Movie> getWatchLater() {
+        return watchLater;
+    }
+
+    public void setWatchLater(List<Movie> watchLater) {
+        this.watchLater = watchLater;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "age=" + age +
+                ", bestFilms=" + bestFilms +
+                ", watchLater=" + watchLater +
+                '}';
+    }
+}
